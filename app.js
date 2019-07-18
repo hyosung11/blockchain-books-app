d0064e19c35267b70fdcdf6c47c90771838fafcc
HyoSung Bidol-Lee <hyosung11@me.com>
2019-07-18 17:01:27 -0400

    getting data from API

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const apiKey = "AIzaSyBWqQ-M9D80N6FOQFj6XC8FwQ2_LrBHU1A"
 // window on load
 $(() => {
 
-const search = $(event.currentTarget).val()
-console.log(search)
+  const searchBooks = (event) => {
+  event.preventDefault()
+  const search = $(event.currentTarget).val()
+  console.log(search)
 
-$.ajax({
-  // retrieves data from API, filter for three categories
-  url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key="+apiKey,
+  $.ajax({
+    // retrieves data from API, filter for three categories
+    url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key="+apiKey,
+  }).then(
+      (data) => {
+        console.log(data)
+      })
+}
 
 
-
-
-
-
-
-
-})
-
+    $('.button').on('click', searchBooks)
 })
