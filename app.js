756816efdf21f15688c97ea5bb05e6c86e66e2c6
HyoSung Bidol-Lee <hyosung11@me.com>
2019-07-18 16:44:12 -0400

    buttons created

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 console.log("connected and ready?")
 
+const apiKey = "AIzaSyBWqQ-M9D80N6FOQFj6XC8FwQ2_LrBHU1A"
+
 // window on load
 $(() => {
+
+const search = $(event.currentTarget).val()
+console.log(search)
+
+$.ajax({
+  // retrieves data from API, filter for three categories
+  url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key="+apiKey,
+
+
+
+
+
+
+
+
+})
 
 })
